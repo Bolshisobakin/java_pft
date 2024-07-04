c68451c9160282edc9ac40a28eb3dfac80560fe9
Bolshisobakin <117268814+Bolshisobakin@users.noreply.github.com>
2024-07-04 14:02:26 +0400

    добавлены функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,26 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hellow world");
+    hello("tru");
+
+    double l=5;
+    System.out.println(area(l));
+
+    double a= 4;
+    double b= 6;
+    System.out.println(area(a,b));
   }
+
+  public static void hello(String somebody) {
+    System.out.println("Hellow " + somebody);
+  }
+
+  public static double area(double len){
+    return len*len;
+  }
+
+  public static double area(double a,double b){
+    return a*b;
+  }
+
 }
